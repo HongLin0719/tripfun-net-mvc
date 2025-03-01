046bc122cdad1ffd625cd843964aa527246d35a0
Joe Liao <gn00401469@hotmail.com>
2025-03-01 15:44:19 +0800

    feat: 新增行程Trip所有靜態頁面

---
+++ Web/wwwroot/js/Trip/input.js
@@ -0,0 +1,20 @@
+﻿// 從 localStorage 獲取選擇的地區
+const selectedRegions = localStorage.getItem('selectedRegions');
+
+// 顯示在頁面上
+document.getElementById('selectedRegionDisplay').textContent = selectedRegions ? selectedRegions : '未選擇區域';
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".searchButton").addEventListener("click", function () {
+        console.log("按鈕被點擊了"); // 確認按鈕被點擊
+        window.location.href = "Search";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".searchButton2").addEventListener("click", function () {
+        console.log("按鈕被點擊了"); // 確認按鈕被點擊
+        window.location.href = "Search";
+    });
+});
